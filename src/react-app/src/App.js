97449dd1d8597ec53d64b1209314a5505f8948d3
annie <wanganni@zhilunkeji.com>
2020-10-19 14:28:12 +0800

    feat: 新增react文件夹

---
+++ src/react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch, Router } from 'react-router-dom';
+import './App.css';
+// import Index from '';
+
+
+function App() {
+  return (
+    <Router>
+      {/* <Route path='path' component={component}/> */}
+    </Router>  
+  );
+}
+
+export default App;
