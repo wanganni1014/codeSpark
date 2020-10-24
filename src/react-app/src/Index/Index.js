ed88570655b66e61c963f29d30707e29117716ed
annie <wanganni@zhilunkeji.com>
2020-10-24 11:09:05 +0800

    feat: hooks

--- src/react-app/src/Index/Index.js
+++ src/react-app/src/Index/Index.js
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, useEffect, useRef } from 'react';
 // function App() {
 //     const [arr, updateArr] = useState([]);
 //     const addList = () => {
@@ -17,48 +17,69 @@ import React, { useState, Component } from 'react';
 //     );
 // }
 
-// export default App;
+function App() {
+    const [count, updateCount] = useState(0);
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            arr: []
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            updateCount(1);
+            getCount();
+        }, 1000);
+        return () => {
+            clearTimeout(timer);
         }
-    }
+    }, []);
 
-    addList = () => {
-        let arr = this.state.arr;
-        arr.push('Hello React');
-        this.setState({
-            arr: arr
-        }, () => {
-            console.log(this.state.arr);
-        });
+    let ref = useRef();
+    ref.current = count;
+    const getCount = () => {
+        console.log(ref.current); // result: 1
     };
-
-    deleteList = () => {
-        const { arr } = this.state;
-        arr.splice(0, 1);
-        this.setState({
-            arr: arr
-        }, () => {
-            console.log(this.state.arr);
-        });
-    };
-
-    render() {
-        const { arr } = this.state;
-        return (
-            <div>
-                {
-                    arr.map((item, index) => (
-                        <p key={index}>{index} {item}</p>
-                    ))
-                }
-            <button onClick={this.addList}>添加List</button>
-            <button onClick={this.deleteList}>删除List</button>
-        </div> 
-        );
-    }
+    
+    return (
+        <div>{count}</div> 
+    );
 }
+
+export default App;
+
+
+// let timer = null;
+// export default class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0
+//         }
+//     }
+
+//    componentDidMount() {
+//         timer = setTimeout(() => {
+//             this.setState({
+//                 count: 1
+//             })
+//             this.getCount();
+//         }, 1000);
+//    }
+
+
+//    getCount = () => {
+//         console.log(this.state.count); // result: 1
+//    }
+
+//    componentWillUnmount() {
+//         clearTimeout(timer);
+//    }
+
+//     render() {
+//         const { count } = this.state;
+//         console.log(count); // result: 1
+//         return (
+//             <div>{count}</div> 
+//         );
+//     }
+// }
+
+
+// 1.[react-hook-usestate-setState](http://www.ptbird.cn/react-hook-usestate-setState.html)
+// 2.[精读《Function Component 入门》](https://juejin.im/post/6844903854174109703)
