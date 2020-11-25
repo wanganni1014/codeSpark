d70b26299f16255b28b40c48e5826000702d3e8b
annie <wanganni@zhilunkeji.com>
2020-11-25 14:23:09 +0800

    ✨ 增加简单算法

--- src/algorithm/03-LeetCode/__test__/index.spec.js
+++ src/algorithm/03-LeetCode/__test__/index.spec.js
@@ -3,3 +3,15 @@ test('反转字符串中的单词',() => {
     const str = "Let's take LeetCode contest";
     expect(reverseWords(str)).toBe("s'teL ekat edoCteeL tsetnoc");
 })
+
+test('计数二进制子串',() => {
+    const { countBinarySubstrings } = require('../countBinarySubstrings')
+    const str = "00110011";
+    expect(countBinarySubstrings(str)).toBe(6);
+})
+
+test('种花问题',() => {
+    const { canPlaceFlowers } = require('../canPlaceFlowers')
+    const flowerbed = [1,0,0,0,1], n = 1
+    expect(canPlaceFlowers(flowerbed, n)).toBe(true);
+})
